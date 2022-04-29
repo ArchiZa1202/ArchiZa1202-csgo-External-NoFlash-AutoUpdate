5499533af618f60f9768707380b95d52d9604b3d
Артур Замотохин <blf_blf_blf3@mail.ru>
2022-04-29 14:44:01 +0500

    ReadSolution

--- MyPrroj/MainWindow.xaml.cs
+++ MyPrroj/MainWindow.xaml.cs
@@ -72,44 +72,28 @@ namespace MyPrroj
            
         }
            
-
-
-        //Метод чита
+       
         private void Esp() 
         {
             while (true)
             {
-                int lPlayer = m.Read<int>(myClient + Offset.dwLocalPlayer);
-                int myTeam = m.Read<int>(lPlayer + Offset.m_iTeamNum);//Команнда игрока
+                int glow = m.Read<int>(myClient + Offset.dwGlowObjectManager); 
                 
-                for (int i = 1; i < 64; i++)
+                
+                for (int i = 1; i < 32; i++)
                 {
-                    int entityList = m.Read<int>(myClient + Offset.dwEntityList + i * 0x10);//id всех игроков
-                    int enemyTeam = m.Read<int>(entityList + Offset.m_iTeamNum);//Команда врага
-                    if (enemyTeam != myTeam && entityList != 0)
+                    int entity = m.Read<int>(myClient + Offset.dwEntityList + i * 0x10);
+                    if (entity != 0) 
                     {
-                        int glowIndex = m.Read<int>(entityList + Offset.m_iGlowIndex);//id glow index
-                        ColormyEnemy(glowIndex, 255, 160, 160);
+                        int entityglowing = m.Read<int>(entity + Offset.m_iGlowIndex);
+                        m.Write(glow + entityglowing * 0x38 + 0x8, 255f);
+                        m.Write(glow + entityglowing * 0x38 + 0xC, 0);
+                        m.Write(glow + entityglowing * 0x38 + 0x10, 0);
+                        m.Write(glow + entityglowing * 0x38 + 0x14, 255f);
+                        m.Write(glow + entityglowing * 0x38 + 0x28, true);
                     }
                 }
             }
-        }
-
-
-        //Метод отрисовки объекта
-        private void ColormyEnemy(int enemyglowIndex, int red, int green, int blue) 
-        {
-            //Представляет глобальные объекты которые могут иметь обводку(glowindex)
-            int glowObject = m.Read<int>(myClient + Offset.dwGlowObjectManager);
-            //Color
-            m.Write(glowObject + (enemyglowIndex * 0x38) + 4, red / 100f);
-            m.Write(glowObject + (enemyglowIndex * 0x38) + 8, green / 100f);
-            m.Write(glowObject + (enemyglowIndex * 0x38) + 12, blue / 100f);
-            //Alpha chanel
-            m.Write(glowObject + (enemyglowIndex * 0x38) + 0x10, 255 / 100f);
-            //health enemy
-            m.Write(glowObject + (enemyglowIndex * 0x38) + 0x24, true);
-            m.Write(glowObject + (enemyglowIndex * 0x38) + 0x25, false);
         }
     }
 }
---
+++ MyPrroj/Memory.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Windows;
+using System.Text;
+
+namespace MyPrroj
+{
+    public class Memory
+    {
+        public static int m_iNumberOfBytesRead = 0;
+        public static int m_iNumberOfBytesWritten = 0;
+        IntPtr processHandle = IntPtr.Zero;
+        IntPtr handle;
+        Process _proc;
+        const int PROCESS_WM_READ = 0x0010;
+        const int PROCESS_VM_WRITE = 0x0020;
+        const int PROCESS_VM_OPERATION = 0x0008;
+        
+        #region OpenProcess
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+        public Memory(string proc)
+        {
+            try
+            {
+                if (proc != null)
+                {
+                    _proc = Process.GetProcessesByName(proc)[0];
+                    IntPtr processHandle = OpenProcess(PROCESS_WM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, false, _proc.Id);
+                    handle = processHandle;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Invalid name Process!");
+            }
+        }
+        #endregion OpenProcess
+
+        #region ReadProcessMemory
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+        public IntPtr Read (IntPtr adresModul)
+        {
+            int bytesRead = Marshal.SizeOf(typeof(IntPtr));
+            byte[] buffer = new byte[bytesRead];
+            
+                ReadProcessMemory(handle, adresModul, buffer, buffer.Length, ref m_iNumberOfBytesRead);
+                return (IntPtr)BitConverter.ToInt32(buffer, 0);                  
+        }
+        #endregion ReadProcessMemory
+
+        #region WriteProcessMemory
+        [DllImport("kernel32.dll", SetLastError = true)]
+
+        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int dwSize, out int lpNumberOfBytesWritten);
+        public void Write(IntPtr adresModul, float f) 
+        {
+            int bytesRead = Marshal.SizeOf(typeof(float));
+            byte[] buffers = StructureToByteArray(bytesRead);
+            WriteProcessMemory(handle, adresModul, buffers, buffers.Length, out m_iNumberOfBytesWritten);
+        }
+
+        public void Write(IntPtr Adress, char[] Value)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(Value);
+
+            WriteProcessMemory(handle, Adress, buffer, buffer.Length, out m_iNumberOfBytesWritten);
+        }
+
+        public void Write(IntPtr Adress, bool flag)
+        {
+            int bytesRead = Marshal.SizeOf(typeof(bool));
+            byte[] buffer = new byte[bytesRead];
+
+            WriteProcessMemory(handle, Adress, buffer, buffer.Length, out m_iNumberOfBytesWritten);
+        }
+        #endregion WriteProcessMemory
+
+
+        private static byte[] StructureToByteArray(object obj)
+        {
+            int length = Marshal.SizeOf(obj);
+
+            byte[] array = new byte[length];
+
+            IntPtr pointer = Marshal.AllocHGlobal(length);
+
+            Marshal.StructureToPtr(obj, pointer, true);
+            Marshal.Copy(pointer, array, 0, length);
+            Marshal.FreeHGlobal(pointer);
+
+            return array;
+        }
+    }
+}
