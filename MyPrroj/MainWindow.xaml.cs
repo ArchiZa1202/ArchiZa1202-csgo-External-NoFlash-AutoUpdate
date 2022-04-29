ac5e58a799e0b696c56bc24e5ad0b3852a0bdaaf
Артур Замотохин <blf_blf_blf3@mail.ru>
2022-04-29 16:13:14 +0500

    finaly version1.0

--- MyPrroj/MainWindow.xaml.cs
+++ MyPrroj/MainWindow.xaml.cs
@@ -1,46 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Diagnostics;
 using System.Threading;
-
 
 namespace MyPrroj
 {
     
     public partial class MainWindow : Window
     {
+        bool flag;
         public static Memory m;
         public static int myClient;
-        public static int myEngine;
         public static bool gesp = false;
         public MainWindow()
         {
             InitializeComponent();
         }
         
-        private void ESP_Proj(object sender, EventArgs e)
-        {
-            gesp = (bool)chEsp.IsChecked;
-        }
+       
         //Поиск процессов и модулей
         private void Run_Procces(object sender, RoutedEventArgs e)
         {
-              
-                while (true)
-            {
-                    Thread.Sleep(1000);
                 try
                 {
                     //Поиск процесса
@@ -52,14 +32,11 @@ namespace MyPrroj
                             if (item.ModuleName == "client.dll") 
                             {
                                 myClient = (int)item.BaseAddress;
-                            }
-                            if (item.ModuleName == "engine.dll")
-                            {
-                                myEngine = (int)item.BaseAddress;
-                            }
+                            } 
                         }
-                        if (myClient != 0 && myEngine != 0) 
+                        if (myClient != 0) 
                         {
+                            flag = true;
                             Esp();
                         }
                 }
@@ -68,32 +45,47 @@ namespace MyPrroj
                     MessageBox.Show("No process found.");
                     return;
                 }
-            }
-           
-        }
-           
-       
+        }    
         private void Esp() 
         {
-            while (true)
+            new Thread(() =>
             {
-                int glow = m.Read<int>(myClient + Offset.dwGlowObjectManager); 
-                
-                
-                for (int i = 1; i < 32; i++)
+                if (flag)
                 {
-                    int entity = m.Read<int>(myClient + Offset.dwEntityList + i * 0x10);
-                    if (entity != 0) 
+                    while (true)
                     {
-                        int entityglowing = m.Read<int>(entity + Offset.m_iGlowIndex);
-                        m.Write(glow + entityglowing * 0x38 + 0x8, 255f);
-                        m.Write(glow + entityglowing * 0x38 + 0xC, 0);
-                        m.Write(glow + entityglowing * 0x38 + 0x10, 0);
-                        m.Write(glow + entityglowing * 0x38 + 0x14, 255f);
-                        m.Write(glow + entityglowing * 0x38 + 0x28, true);
+                        Thread.Sleep(1);
+                        int lPlayer = m.Read<int>(myClient + Offset.dwLocalPlayer);
+                        int playrTeam = m.Read<int>(lPlayer + Offset.m_iTeamNum);
+                        int glow = m.Read<int>(myClient + Offset.dwGlowObjectManager);
+
+
+                        for (int i = 1; i < 32; i++)
+                        {
+                            int entity = m.Read<int>(myClient + Offset.dwEntityList + i * 0x10);
+                            int enemyTeam = m.Read<int>(entity + Offset.m_iTeamNum);
+                            if (entity != 0)
+                            {
+                                if (enemyTeam != 0 && enemyTeam != playrTeam)
+                                {
+                                    int entityglowing = m.Read<int>(entity + Offset.m_iGlowIndex);
+                                    m.Write(glow + entityglowing * 0x38 + 0x8, 255f);
+                                    m.Write(glow + entityglowing * 0x38 + 0xC, 0);
+                                    m.Write(glow + entityglowing * 0x38 + 0x10, 0);
+                                    m.Write(glow + entityglowing * 0x38 + 0x14, 255f);
+                                    m.Write(glow + entityglowing * 0x38 + 0x28, true);
+                                }
+                            }
+                        }
+                        if (!flag) return;
                     }
                 }
-            }
+            }).Start();
+        }
+
+        private void btnReturn_Click(object sender, RoutedEventArgs e)
+        {
+            flag = false;
         }
     }
 }
